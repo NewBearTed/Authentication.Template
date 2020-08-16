03b4db43223d7eedceca2a27a44effb725bbf3bb
NewBearTed <xprogramador.net@gmail.com>
2020-08-16 17:11:50 -0300

    Project Initial Adding Authentication Add Identity Migrations

---
+++ src/MVC/Authentication.Project/Authentication.Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authentication.Project.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>().ToTable("Roles");
+            builder.Entity<IdentityUser>().ToTable("Users");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens");
+        }
+    }
+}
---
+++ src/MVC/Authentication.Project/Authentication.Project/ReadMe.txt
@@ -0,0 +1,8 @@
+﻿1-	dotnet ef migrations add InitialCreate --project Authentication.Project
+
+2-	dotnet ef database update --project Authentication.Project
+
+--------- E-mail confirm -----------
+	Obs:https://sendgrid.com/
+1-	dotnet user-secrets set SendGridUser RickAndMSFT
+2-	dotnet user-secrets set SendGridKey <key>
